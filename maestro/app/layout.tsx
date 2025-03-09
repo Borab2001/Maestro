e6724fcbffc4a441c0083175fd450f3d1c266f7a
Bora <borabalos9@gmail.com>
2025-03-09 20:22:44 +0100

    feat: Configured lenis for smooth scrolling

--- maestro/app/layout.tsx
+++ maestro/app/layout.tsx
@@ -1,36 +1,42 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import Header from "@/components/header";
+import ScrollProvider from "@/lib/scroll-provider";
+
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Maestro",
-  description: "/",
+	title: "Maestro",
+	description: "/",
 };
 
+
 export default function RootLayout({
-  children,
+  	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<ScrollProvider>
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<Header />
+					{children}
+				</body>
+			</ScrollProvider>
+		</html>
+	);
 }
