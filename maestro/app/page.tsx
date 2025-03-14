6364aff5000a8c385e12b2ef6445df8af730cb63
Bora <borabalos9@gmail.com>
2025-03-14 14:24:29 +0100

    style: Redesign of home page hero section

--- maestro/app/page.tsx
+++ maestro/app/page.tsx
@@ -2,58 +2,79 @@
 
 import Image from "next/image";
 
-import { useRef } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import SplitType from "split-type";
+// import { useRef } from "react";
+// import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import SplitType from "split-type";
 
-gsap.registerPlugin(SplitType);
+// gsap.registerPlugin(SplitType);
 
 
 export default function Home() {
-	const container = useRef(null);
+	// const container = useRef(null);
 	
-	useGSAP(() => {
-		const heroText = new SplitType("h1", { types: "words" });
-		gsap.set(heroText.words, { y: 400 });
+	// useGSAP(() => {
+	// 	const heroText = new SplitType("h1", { types: "words" });
+	// 	gsap.set(heroText.words, { y: 400 });
 
-		gsap.to(heroText.words, {
-			y: 0,
-			duration: 1,
-			stagger: 0.075,
-			ease: "power4.out",
-			delay: 1
-		})
-	},{ scope: container });
+	// 	gsap.to(heroText.words, {
+	// 		y: 0,
+	// 		duration: 1,
+	// 		stagger: 0.075,
+	// 		ease: "power4.out",
+	// 		delay: 1
+	// 	})
+	// },{ scope: container });
 
 
 	return (
-		<main className="w-screen min-h-screen" ref={container}>
-			<div className="w-full flex flex-col pt-[40vh]">
-				<div className="grid grid-cols-5 grid-rows-2">
-					<div className="col-span-2" />
-					{/* <h1 
-						className="text-[20vw] font-semibold tracking-[-0.5rem] leading-none [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]"
-					>
-						Maestro
-					</h1> */}
-					<div className="col-span-2 row-span-1 h-auto">
-						<h1 
-							className="text-2xl font-medium leading-relaxed [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]"
-						>
-							We are a group of artist between the age of 18 and 28, coming from all corners of france. We all have one goal in common- reinventing music to continue to make it live.
-						</h1>
-					</div>
+		// <main className="w-screen min-h-screen" ref={container}>
+		// 	<div className="w-full flex flex-col pt-[40vh]">
+		// 		<div className="grid grid-cols-5 grid-rows-2">
+		// 			<div className="col-span-2" />
+		// 			{/* <h1 
+		// 				className="text-[20vw] font-semibold tracking-[-0.5rem] leading-none [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]"
+		// 			>
+		// 				Maestro
+		// 			</h1> */}
+		// 			<div className="col-span-2 row-span-1 h-auto">
+		// 				<h1 
+		// 					className="text-2xl font-medium leading-relaxed [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]"
+		// 				>
+		// 					We are a group of artist between the age of 18 and 28, coming from all corners of france. We all have one goal in common- reinventing music to continue to make it live.
+		// 				</h1>
+		// 			</div>
+		// 		</div>
+		// 		<div className="col-span-1" />
+		// 		<Image 
+		// 			src={"/images/group.webp"} 
+		// 			alt="Maestro group shot of first concert" 
+		// 			width={1920} 
+		// 			height={1080} 
+		// 			className="w-full aspect-[16/9] object-cover object-center" 
+		// 		/>
+		// 	</div>
+		// </main>
+		<main className="min-h-screen pt-[72px]">
+			 <section className="px-4 md:px-8 py-6 md:py-12">
+				<h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium">La Musique Réinventée</h1>
+			</section>
+
+			<section className="flex flex-col md:flex-row px-4 md:px-8 lg:px-16 py-8">
+				<div className="md:w-3/4 relative aspect-[16/12]">
+					<Image 
+						src="/images/group2.webp" 
+						alt="Groupe de musiciens Maestro" 
+						fill
+						className="object-cover"
+					/>
 				</div>
-				<div className="col-span-1" />
-				<Image 
-					src={"/images/group.webp"} 
-					alt="Maestro group shot of first concert" 
-					width={1920} 
-					height={1080} 
-					className="w-full aspect-[16/9] object-cover object-center" 
-				/>
-			</div>
+				<div className="md:w-1/2 md:pl-8 py-6">
+					<p className="text-sm md:text-base mb-6">
+						Nous sommes une troupe d&apos;artistes, réunis par une passion commune : repousser les limites de la scène musicale. Nous jouons à un, à deux, à trois... jusqu&apos;à neuf sur scène. Entre compositions, improvisations et interactions avec le public, chaque spectacle est une expérience unique.
+					</p>
+				</div>
+			</section>
 		</main>
 	);
 }
