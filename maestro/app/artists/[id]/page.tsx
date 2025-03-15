40aa994ed16ad46406b1584f40b5e164a8fa0589
Bora <borabalos9@gmail.com>
2025-03-15 02:43:13 +0100

    feat: Mapping artist socials from json file

--- maestro/app/artists/[id]/page.tsx
+++ maestro/app/artists/[id]/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import artists from "@/data/artists.json";
 import { notFound, useParams } from "next/navigation";
+import Link from "next/link";
 
 
 const Artist = () => {
@@ -17,28 +18,36 @@ const Artist = () => {
 
     return (
         <div className="min-h-screen px-4 md:px-8 pt-[72px]">
-            <div className="grid grid-cols-2">
+            <section className="pb-8 grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div className="flex">
                     <Image
                         src={artist.image}
                         alt={`${artist.name} portrait`}
                         width={1920}
                         height={1080}
-                        className="w-full h-[calc(100vh-72px)] flex-1 rounded-lg object-cover object-center"
+                        className="w-full h-[calc(100vh-104px)] flex-1 rounded-lg object-cover object-center"
                     />
                 </div>
-                <div className="pt-18 px-8 flex flex-col gap-24 items-start justify-center">
+                <div className="pt-18 flex flex-col gap-24 items-start justify-end">
                     <h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
                         {artist.name}
                     </h1>
                     <p className="font-medium text-base leading-loose mb-6 [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
                         {artist.bio}
 					</p>
-                    <div className="flex flex-row items-center justify-between gap-4">
-                        {/* socials here */}
+                    <div className="w-full flex flex-row items-center justify-between gap-8">
+                        {artist.socials.map((social, index) => (
+                            <div key={index} className="flex flex-col items-start gap-4">
+                                <span className="text-lg md:text-xl font-medium capitalize">{social.platform}</span>
+                                <Link href={social.url} target="_blank" rel="noopener noreferrer" className="text-secondary text-base md:text-lg font-medium">
+                                    {social.username}
+                                </Link>
+                            </div>
+                        ))}
                     </div>
                 </div>
-            </div>
+            </section>
+
             <div className="w-[70%] pt-48 px-4 sm:px-8 md:px-20 flex flex-col gap-4">
                 <h2 className="text-3xl font-medium">Artist Background</h2>
                 <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repellendus, harum dolorem tempore, eius aperiam recusandae voluptate omnis dolorum soluta provident voluptatibus error dignissimos saepe sequi similique ea voluptas nam?</p>
