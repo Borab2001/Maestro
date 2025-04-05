8246cc43d71973d0811a5315fd96c3b1a4c4f0e1
Bora <borabalos9@gmail.com>
2025-04-05 03:22:41 +0200

    feat: Vertical tiles added to home page for test

--- maestro/app/page.tsx
+++ maestro/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 // import dynamic from 'next/dynamic';
 import InfiniteGallery from "@/components/ui/infinite-gallery";
+import VerticalTiles from "@/components/vertical-tiles";
 
 // import { useRef } from "react";
 // import gsap from "gsap";
@@ -58,69 +59,77 @@ export default function Home() {
 		// 		/>
 		// 	</div>
 		// </main>
-		<main className="min-h-screen pt-[72px]">
-			 <section className="px-4 md:px-8 py-6 md:py-12">
-				<h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium">La Musique Réinventée</h1>
-			</section>
+		<VerticalTiles
+			animationDelay={1}
+			animationDuration={1.5}
+			minTileWidth={64}
+			stagger={0.05}
+			tileClassName="bg-gradient-to-r from-zinc-100 to-zinc-300 z-10000"
+		>
+			<main className="min-h-screen pt-[72px]">
+				<section className="px-4 md:px-8 py-6 md:py-12">
+					<h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium">La Musique Réinventée</h1>
+				</section>
 
-			<section className="flex flex-col md:flex-row px-4 md:px-8 lg:px-16 py-8">
-				<div className="md:w-3/4 relative rounded-lg aspect-[16/12]">
-					<Image 
-						src="/images/group2.webp" 
-						alt="Groupe de musiciens Maestro" 
-						fill
-						className="object-cover"
-						priority
-					/>
-				</div>
-				<div className="md:w-1/2 md:pl-8 py-6">
-					<p className="text-base leading-loose mb-6">
-						Nous sommes une troupe d&apos;artistes, réunis par une passion commune : repousser les limites de la scène musicale. Nous jouons à un, à deux, à trois... jusqu&apos;à neuf sur scène. Entre compositions, improvisations et interactions avec le public, chaque spectacle est une expérience unique.
-					</p>
-				</div>
-			</section>
-			
-			<section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 py-8">
-				<div className="col-span-1">
-					<p className="text-base leading-loose">
-						L&apos;idée de créer cette troupe est née il y a plusieurs mois, avant d&apos;aboutir, il y a environ un an. Tout est parti d&apos;échanges avec d&apos;autres musiciens. Certains viennent d&apos;écoles de musique, d&apos;autres proviennent du conservatoire, d&apos;autres des genres et même autodidacte.
-					</p>
-				</div>
+				<section className="flex flex-col md:flex-row px-4 md:px-8 lg:px-16 py-8">
+					<div className="md:w-3/4 relative rounded-lg aspect-[16/12]">
+						<Image 
+							src="/images/group2.webp" 
+							alt="Groupe de musiciens Maestro" 
+							fill
+							className="object-cover"
+							priority
+						/>
+					</div>
+					<div className="md:w-1/2 md:pl-8 py-6">
+						<p className="text-base leading-loose mb-6">
+							Nous sommes une troupe d&apos;artistes, réunis par une passion commune : repousser les limites de la scène musicale. Nous jouons à un, à deux, à trois... jusqu&apos;à neuf sur scène. Entre compositions, improvisations et interactions avec le public, chaque spectacle est une expérience unique.
+						</p>
+					</div>
+				</section>
 				
-				<div className="col-span-1">
-					<p className="text-base leading-loose">
-						At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.
+				<section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 py-8">
+					<div className="col-span-1">
+						<p className="text-base leading-loose">
+							L&apos;idée de créer cette troupe est née il y a plusieurs mois, avant d&apos;aboutir, il y a environ un an. Tout est parti d&apos;échanges avec d&apos;autres musiciens. Certains viennent d&apos;écoles de musique, d&apos;autres proviennent du conservatoire, d&apos;autres des genres et même autodidacte.
+						</p>
+					</div>
+					
+					<div className="col-span-1">
+						<p className="text-base leading-loose">
+							At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.
+						</p>
+					</div>
+				</section>
+
+				<section className="py-16 md:py-64 px-4 md:px-8 lg:px-16 text-center">
+					<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-20 sm:mb-24 md:mb-32">Maestro, c&apos;est...</h2>
+					
+					<div className="grid grid-cols-1 sm:grid-cols-3 gap-16 sm:gap-8">
+						<div>
+							<p className="text-6xl lg:text-[80px] font-medium">2</p>
+							<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">concerts</p>
+						</div>
+						<div>
+							<p className="text-6xl lg:text-[80px] font-medium">500</p>
+							<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">places vendues</p>
+						</div>
+						<div>
+							<p className="text-6xl lg:text-[80px] font-medium">20</p>
+							<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">artistes</p>
+						</div>
+					</div>
+				</section>
+
+				<section className="px-4 md:px-8 py-6 md:py-12">
+					<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-6">Titre ici</h2>
+					<p className="max-w-3xl text-base leading-loose">
+						At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.
 					</p>
-				</div>
-			</section>
+				</section>
 
-			<section className="py-16 md:py-64 px-4 md:px-8 lg:px-16 text-center">
-				<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-20 sm:mb-24 md:mb-32">Maestro, c&apos;est...</h2>
-				
-				<div className="grid grid-cols-1 sm:grid-cols-3 gap-16 sm:gap-8">
-					<div>
-						<p className="text-6xl lg:text-[80px] font-medium">2</p>
-						<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">concerts</p>
-					</div>
-					<div>
-						<p className="text-6xl lg:text-[80px] font-medium">500</p>
-						<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">places vendues</p>
-					</div>
-					<div>
-						<p className="text-6xl lg:text-[80px] font-medium">20</p>
-						<p className="text-2xl md:text-3xl lg:text-[40px] mt-2 font-medium text-secondary">artistes</p>
-					</div>
-				</div>
-			</section>
-
-			<section className="px-4 md:px-8 py-6 md:py-12">
-				<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-6">Titre ici</h2>
-				<p className="max-w-3xl text-base leading-loose">
-					At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.
-				</p>
-			</section>
-
-			<InfiniteGallery />
-		</main>
+				<InfiniteGallery />
+			</main>
+		</VerticalTiles>
 	);
 }
