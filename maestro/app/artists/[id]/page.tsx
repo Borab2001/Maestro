538271b698c7e6772a658deb2686e5a855b71517
Bora <borabalos9@gmail.com>
2025-04-06 19:21:56 +0200

    feat: Artist page getting data from json file and passing to timeline component

--- maestro/app/artists/[id]/page.tsx
+++ maestro/app/artists/[id]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import artists from "@/data/artists.json";
 import { notFound, useParams } from "next/navigation";
 import Link from "next/link";
+import { Timeline } from "@/components/ui/timeline";
 
 
 const Artist = () => {
@@ -15,6 +16,18 @@ const Artist = () => {
     if (!artist) {
         return notFound();
     }
+
+    const timelineData = artist.timeline?.map((item) => ({
+        year: `${item.year}`,
+        title: `${item.title}`,
+        description: `${item.description}`,
+        // content: (
+        //     <div className="bg-neutral-50 dark:bg-neutral-900 p-4 rounded-lg border border-neutral-200 dark:border-neutral-800">
+        //         <h4 className="text-lg font-semibold mb-2">{item.title}</h4>
+        //         <p className="text-sm text-neutral-600 dark:text-neutral-400">{item.description}</p>
+        //     </div>
+        // ),
+    })) || [];
 
     return (
         <div className="min-h-screen px-4 md:px-8 pt-[72px]">
@@ -48,9 +61,12 @@ const Artist = () => {
                 </div>
             </section>
 
-            <div className="w-[70%] py-48 flex flex-col gap-4 md:gap-8">
+            <div className="w-full md:w-[70%] py-48 flex flex-col gap-4 md:gap-8">
                 <h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-6">Background</h2>
                 <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repellendus, harum dolorem tempore, eius aperiam recusandae voluptate omnis dolorum soluta provident voluptatibus error dignissimos saepe sequi similique ea voluptas nam?</p>
+                {timelineData.length > 0 && (
+                    <Timeline data={timelineData} />
+                )}
             </div>
 
             <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
