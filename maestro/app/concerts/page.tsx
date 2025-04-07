0c993de853fad4d7672c71197518081651a52959
Bora <borabalos9@gmail.com>
2025-04-07 20:00:25 +0200

    feat: First section of concerts page

--- maestro/app/concerts/page.tsx
+++ maestro/app/concerts/page.tsx
@@ -2,33 +2,13 @@
 
 const Concerts = () => {
     return (
-        <div className="min-h-screen pt-48 px-4 sm:px-8 md:px-20">
-            {/* <div className="mx-auto w-full max-w-3xl flex flex-col gap-4">
-                <Image 
-                    src={"/images/group.webp"} 
-                    alt="Maestro group shot of first concert" 
-                    width={1920} 
-                    height={1080} 
-                    className="w-full object-cover object-center" 
-                />
-                <div className="grid grid-cols-2 gap-4">
-                    <Image 
-                        src={"/images/group.webp"} 
-                        alt="Maestro group shot of first concert" 
-                        width={1920} 
-                        height={1080} 
-                        className="w-full object-cover object-center" 
-                    />
-                    <Image 
-                        src={"/images/group.webp"} 
-                        alt="Maestro group shot of first concert" 
-                        width={1920} 
-                        height={1080} 
-                        className="w-full object-cover object-center" 
-                    />
+        <div className="min-h-screen px-4 md:px-8 pt-[72px]">
+            <section className="py-6 md:py-12">
+                <div className="w-full md:w-[70%] max-w-5xl pb-48 flex flex-col gap-4 md:gap-8">
+                    <h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium mb-6">Concerts</h1>
+                    <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repellendus, harum dolorem tempore, eius aperiam recusandae voluptate omnis dolorum soluta provident voluptatibus error dignissimos saepe sequi similique ea voluptas nam?</p>
                 </div>
-            </div> */}
-            In progress
+            </section>
         </div>
     );
 }
