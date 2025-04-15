981ccc6d5e93120a68ed9b228f4e4120eb8298a0
Bora <borabalos9@gmail.com>
2025-04-15 15:54:50 +0200

    feat: Homepage texts updated

--- maestro/app/page.tsx
+++ maestro/app/page.tsx
@@ -129,7 +129,10 @@ export default function Home() {
 				</div>
 				<div className="md:w-1/2 md:pl-8 py-6">
 					<p className="text-base leading-loose mb-6 [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
-						Nous sommes une troupe d&apos;artistes, réunis par une passion commune : repousser les limites de la scène musicale. Nous jouons à un, à deux, à trois... jusqu&apos;à neuf sur scène. Entre compositions, improvisations et interactions avec le public, chaque spectacle est une expérience unique.
+					Nous sommes la première troupe de pianistes en France et nous avons entre 18 et 28 ans.
+					Le piano est souvent perçu comme un art solitaire, mais nous le transformons en expérience collective.
+					Notre concept ? Mélanger nos parcours – conservatoire, gares ou autodidacte – et proposer un format inédit
+					où la mise en scène et l’univers visuel sont aussi puissants que la musique. Le piano peut être un vrai spectacle vivant.
 					</p>
 				</div>
 			</section>
@@ -137,13 +140,19 @@ export default function Home() {
 			<section className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-8 py-8">
 				<div className="col-span-1">
 					<p className="text-base leading-loose [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
-						L&apos;idée de créer cette troupe est née il y a plusieurs mois, avant d&apos;aboutir, il y a environ un an. Tout est parti d&apos;échanges avec d&apos;autres musiciens. Certains viennent d&apos;écoles de musique, d&apos;autres proviennent du conservatoire, d&apos;autres des genres et même autodidacte.
+						Nos concerts mélangent musiques de film, pop, classiques et compositions personnelles. Chaque performance est pensée pour
+						raconter une histoire à la fois musicale et visuelle à travers un voyage émotionnel. Nous improvisons, interagissons avec
+						le public, et créons des ponts entre les styles pour créer un format hybride qui mêle le meilleur de la scène et des gares.
+						C&apos;est en construisant avec une liberté totale que ce projet est devenu réalité.
 					</p>
 				</div>
 				
 				<div className="col-span-1">
 					<p className="text-base leading-loose [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
-						At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.
+						Certains d&apos;entre nous sont très suivis sur les réseaux sociaux, mais, n&apos;étant pas issus d&apos;un parcours traditionnel,
+						ils ressentent parfois un manque de légitimité à l&apos;idée de monter sur scène. Pourtant, leur talent est indéniable.
+						Nous voulons montrer qu&apos;il est possible de passer des réseaux sociaux aux grandes scènes, du virtuel au réel,
+						montrant une unité artistique et humaine, venant des quatre coins de la France, qui donne toute sa force à notre projet.
 					</p>
 				</div>
 			</section>
@@ -168,9 +177,18 @@ export default function Home() {
 			</section>
 
 			<section className="px-4 md:px-8 py-6 md:py-12">
-				<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-6 [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">Titre ici</h2>
-				<p className="max-w-3xl text-base leading-loose [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
-					At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident. At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas.
+				<h2 className="text-3xl md:text-6xl lg:text-7xl font-medium mb-6 [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
+					L&apos;avenir du Projet
+				</h2>
+				<p className="max-w-4xl text-base leading-loose [clip-path:polygon(0_0,_100%_0,_100%_100%,_0_100%)]">
+					Grâce à notre travail collectif, nous avons donné nos premiers concerts cette année, et les retours nous encouragent d&apos;autant plus à continuer.
+					Mais l&apos;histoire ne fait que commencer. En plus de nos concerts, nous rêvons de nous diversifier, de créer notre propre musique,
+					de fusionner les arts, de multiplier les collaborations, de créer des ponts entre les styles musicaux.
+					<br />
+					<br />
+					Nous voulons repousser les limites et prouver qu&apos;un instrument classique peut porter des messages forts et actuels.
+					Nous aimerions aussi étendre notre présence au-delà de Paris, jouer dans des villes où la musique résonne différemment et
+					faire découvrir notre univers à un public de plus en plus large, tout en restant fidèles à ce qui nous fait vibrer.
 				</p>
 			</section>
 
