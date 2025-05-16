fd88b11cc10d09273e9ac5abc2e5a92c99b29949
Bora <borabalos9@gmail.com>
2025-05-16 14:29:06 +0200

    style: Adjusted grid in artists page

--- maestro/app/artists/page.tsx
+++ maestro/app/artists/page.tsx
@@ -19,10 +19,10 @@ const Artists = () => {
             <section className="py-6 md:py-12">
                 <h1 className="text-3xl md:text-6xl lg:text-[80px] font-medium">Les Artistes</h1>
             </section>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
                 {artists.map((artist) => (
                     <Link 
-                        className='group relative w-full aspect-[3/5] overflow-hidden rounded-lg'
+                        className='group relative w-full aspect-[3/4] md:aspect-[3/4] lg:aspect-[2/3] xl:aspect-[3/5] overflow-hidden rounded-lg'
                         key={artist.id}
                         href={`/artists/${artist.id}`}
                         onClick={(e) => {
