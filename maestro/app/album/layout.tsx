2788cece1eb0eea5493a83e6860fd645534cb6e7
Bora <borabalos9@gmail.com>
2026-03-29 23:14:25 +0200

    fix: Og image alt text

--- maestro/app/album/layout.tsx
+++ maestro/app/album/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
         url: "/og-image.png",
         width: 1200,
         height: 630,
-        alt: "Maestro Albums",
+        alt: "Maestro Troupe",
       },
     ],
   },
--- maestro/app/layout.tsx
+++ maestro/app/layout.tsx
@@ -61,7 +61,7 @@ export const metadata: Metadata = {
 			url: "/og-image.png",
 			width: 1200,
 			height: 630,
-			alt: "Maestro Concerts Preview",
+			alt: "Maestro Troupe",
 			},
 		],
 	},
